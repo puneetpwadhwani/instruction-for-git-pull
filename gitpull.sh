98ea3dabc20cbd4b855b8ae4139b9ae904f8cf30
Puneet Wadhwani <puneetpwadhwani@gmail.com>
2024-01-27 18:19:33 +0530

    Create gitpull.sh

---
+++ gitpull.sh
@@ -0,0 +1,11 @@
+add private key /root/.ssh/id_rsa
+install git(if git not install)
+go to build folder 
+git init
+git pull "git repo"
+
+crontab sh file
+cd /root
+nano git.sh
+cd /var/www/"buildpath"/
+git pull "gitrepo"
